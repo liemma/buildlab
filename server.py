9acad39187ac863c3668d872e1ad3f17a7ea5d03
liemma <57811437+liemma@users.noreply.github.com>
2025-04-04 21:27:27 -0400

    Made edits

--- server.py
+++ server.py
@@ -40,16 +40,18 @@ scoreboard = [
 def show_scoreboard():
     return render_template('scoreboard.html', scoreboard = scoreboard) 
 
-@app.route('/increase_score', methods=['GET', 'POST'])
+@app.route('/increase_score', methods=['POST'])
 def increase_score():
     global scoreboard
+    json_data = request.get_json()
+    team_id = json_data["id"]
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
-    
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
+
+    # Sort by score descending
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
 
     return jsonify(scoreboard=scoreboard)
 
